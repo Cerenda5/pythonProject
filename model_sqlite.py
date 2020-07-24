bdda9ffc175e5f088353ff55c6bc98fbcf6172b9
Cerenda5 <linda.dogan@outlook.fr>
2020-07-24 13:10:16 +0200

    Partie 2 done

---
+++ model_sqlite.py
@@ -0,0 +1,135 @@
+import sqlite3
+
+def createTables():
+    conn = sqlite3.connect('shareCodeBdd.db')
+    c = conn.cursor()
+
+    #c.execute("DROP TABLE code")
+    #c.execute("DROP TABLE edition")
+
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS code (
+            uid INTEGER PRIMARY KEY AUTOINCREMENT,
+            code TEXT DEFAULT 'Insert your code here ...',
+            lang VARCHAR(50) DEFAULT 'py',
+            createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updatedAt TIMESTAMP
+        )
+    ''')
+
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS edition (
+            uid INTEGER PRIMARY KEY AUTOINCREMENT,
+            ip INT,
+            user_agent VARCHAR(255),
+            TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+def createCode():
+    conn = sqlite3.connect('shareCodeBdd.db')
+    c = conn.cursor()
+
+    c.execute('''
+        INSERT INTO code DEFAULT VALUES
+    ''')
+
+    uid = c.lastrowid
+
+    conn.commit()
+    conn.close()
+
+    return uid
+
+def getCode(uid):
+    conn = sqlite3.connect('shareCodeBdd.db')
+    c = conn.cursor()
+
+    c.execute('''
+        SELECT
+            code,
+            lang
+        FROM code
+        WHERE uid = ?
+    ''', uid)
+
+    result = c.fetchone()
+
+    conn.commit()
+    conn.close()
+
+    return result
+
+def getAllCode():
+    conn = sqlite3.connect('shareCodeBdd.db')
+    c = conn.cursor()
+
+    c.execute('''
+        SELECT
+            uid,
+            code,
+            lang
+        FROM code
+        ORDER BY
+            updatedAt DESC,
+            createdAt DESC
+    ''')
+
+    result = c.fetchall()
+
+    conn.commit()
+    conn.close()
+
+    return result
+
+def updateCode(uid, code, lang):
+    conn = sqlite3.connect('shareCodeBdd.db')
+    c = conn.cursor()
+
+    result = c.execute('''
+        UPDATE code
+        SET
+            code= ?,
+            lang = ?,
+            updatedAt = CURRENT_TIMESTAMP
+        WHERE uid = ?
+    ''', (code, lang, uid))
+
+    conn.commit()
+    conn.close()
+
+    return result
+
+def createEdition(ip, user_agent):
+    conn = sqlite3.connect('shareCodeBdd.db')
+    c = conn.cursor()
+
+    c.execute('''
+        INSERT INTO edition
+        VALUES(?,?)
+    ''', (ip, user_agent))
+
+    uid = c.lastrowid
+
+    conn.commit()
+    conn.close()
+
+    return uid
+
+def getEdition():
+    conn = sqlite3.connect('shareCodeBdd.db')
+    c = conn.cursor()
+
+    result = c.execute('''
+        SELECT ip, user_agent, date
+        FROM edition
+        ORDER BY date DESC
+    ''')
+
+    conn.commit()
+    conn.close()
+
+    return result
